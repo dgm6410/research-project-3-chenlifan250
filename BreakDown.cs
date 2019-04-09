6ef035f7875ac82440543f6c349865b9de653ef7
Jacky <chen.lif@husky.neu.edu>
2019-04-09 19:32:55 -0400

    Add files via upload

---
+++ BreakDown.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakDown : MonoBehaviour
+{
+
+    float burnAmounting;
+    bool burningFlag;
+    public float burningSpeed; 
+    public Animation anim;
+    public Vector3 speed;
+    public GameObject material;
+
+    // Use this for initialization
+    void Start()
+    {
+        burnAmounting = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (burningFlag)
+        {
+            burnAmounting += Time.deltaTime * burningSpeed;
+            material.GetComponent<Renderer>().material.SetFloat("_BurnAmount", burnAmounting);
+            if (burnAmounting >= 1)
+            {
+                burningFlag = false;
+                if (GetComponent<Rigidbody>())
+                {
+                    GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+                }
+                else {
+                    Destroy(gameObject);
+                }
+
+                if (transform.position.z <= -7f)
+                {
+                    Destroy(gameObject);
+                }
+            }
+
+        
+
+
+        }
+        if (transform.position.z <= -7f) {
+            attack();
+        }
+
+        transform.Translate(Time.deltaTime * speed);
+
+
+    }
+
+    void breakDown()
+    {
+        burnAmounting = 0;
+        burningFlag = true;
+        if (anim) {
+            anim.Stop();
+        }
+        
+        speed = new Vector3(0, 0, 0);
+    }
+
+    void OnMouseDown()
+    {
+        breakDown();
+    }
+
+    void attack() {
+        speed = new Vector3();
+        if (anim)
+        {
+            anim.Play("attack");
+        }
+    }
+}
